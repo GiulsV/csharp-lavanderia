757441a7bf353f7236409db7bb8fc5a792860b85
GiulsV <valente.giulia93@gmail.com>
2022-11-03 14:22:29 +0100

    Dettagli essercizio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 
+/* Lavanderia con 
+    - 5 lavatrici
+    - 5 ascigatrici
+    - Costo gettoni: 0.5 cent * gettone
+    - Capacità lavatrice: max 1 l detersivo
+                          max 500 ml ammorbidente
+
+    Programmi lavatrice:
+    1) RINFRESCANTE: 
+            costo: 2 gettoni
+            tempo: 20 min
+            consumo: 200 ml detersivo, 5 ml ammorbidente
+    2) RINNOVANTE:
+            costo: 3 gettoni
+            tempo: 40 min
+            consumo: 40 ml detersivo, 10 ml ammorbidente
+    3) SGRASSANTE:
+            costo: 4 gettoni
+            tempo: 60 min
+            cosumo: 60 ml detersivo, 15 ml ammorbidente
+    Programmi asciugatrice:
+    1)RAPIDO: 
+        costo: 2 gettoni
+        tempo: 30 min
+    2)INTENSO: 
+        costo: 4 gettoni
+        tempo: 60 min                   
+ */
+
+/*
+ Sistema di controllo:
+    1) Stato di utilizzo: elenco macchine che sono in funzione e ferme
+    2) Dettaglio macchine: info nome, stato,tipo di lavaggio, qnt detersivo, durata lavaggio, tempo alla fine
+    3) Incasso
+ */
